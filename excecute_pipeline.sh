b637481fc77bf112cd521e4fcaf8d6902970bd99
ChaosDonkey06 <je.cascante10@uniandes.edu.co>
2020-09-23 21:53:35 +0000

    adding turn off auto

--- excecute_pipeline.sh
+++ excecute_pipeline.sh
@@ -1,3 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:pipeline_scripts/functions/
 export PYTHONPATH=$PYTHONPATH:pipeline_scripts/
-python pipeline_scripts/excecute_all.py 
+python pipeline_scripts/excecute_all.py
+
+turn_off_instance=$1
+if turn_off_instance
+then
+    sudo shutdown -h now
+fi
