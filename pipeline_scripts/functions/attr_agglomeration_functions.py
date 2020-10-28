2fee84b16b53207d86fc107b56b0b79683298a45
ap3rg <ap3rg@virginia.edu>
2020-10-28 15:12:11 +0100

    agglomerator finished

--- pipeline_scripts/functions/attr_agglomeration_functions.py
+++ pipeline_scripts/functions/attr_agglomeration_functions.py
@@ -10,6 +10,15 @@ import scipy.stats as stats
 from global_config import config
 data_dir = config.get_property('data_dir')
 
+'''
+GONCHE:
+Hice estos cambios:
+    (1) Agregue al dict de response el nombre del atributo principal para poder ejecutar las funciones con atributo principal y secundario
+    (2) Cuando se llegaba al merge de "Agglomerates columns with multiple parameters" fallaba si el df_response no se habia inicializado antes,
+        es decir, cuando el único atributo era de este tipo. Inclui un check para ver si el df_response seguia siendo pd.series para inicializarlocomo DF.
+    (3) Agregue parametros por defecto para las funciones y una funcion para setear los parametros que se llama get_params().
+'''
+
 
 # Identation variable
 ident = '            '
@@ -58,10 +67,13 @@ def agglomerate(df, aggl_scheme, groupby_cols, agglomerate_cols):
         if not functions[col]['single_attribute']:
             fun = get_corresponding_function(functions[col])
             df_temp = df.groupby(groupby_cols).apply(fun).to_frame().reset_index()
+            
             # renames the new column
             df_temp.rename(columns = {0:col}, inplace = True)
-            
+
             # Merges
+            if isinstance(df_response, pd.Series):
+                df_response = pd.DataFrame(df_response)
             df_response = df_response.merge(df_temp, on = groupby_cols)
             
     
@@ -95,8 +107,20 @@ def get_corresponding_function_declaration(col, aggl_scheme):
     # Check if the agglomeraton requires multiple parameters
     response['single_attribute'] = pd.isna(response['secondary_attr'])
     
+    # Add name of original attr
+    response['attr_name'] = col
     return(response)
 
+# Parameter loading Mehtod
+def get_params(default_params, input_params):
+    for p in input_params:
+        key = p.split("=")[0]
+        value = p.split("=")[1]
+        if key not in list(default_params.keys()):
+            raise Exception(f"Parameter key {key} not found in allowed parameters")
+        default_params[key] = value
+
+    return default_params
 
 
 # Organizer Method
@@ -108,7 +132,22 @@ def get_corresponding_function(function_declaration):
     '''
     
     name = function_declaration['aggl_function']
+
+    # If the function requires parameters, they must all be specified. Parameters that are not
+    # specified in this dict will raise an Exception
+    default_params = {'attr_append': {"sep": "|",
+                                    "null_handling":"drop_na",
+                                    "fill_null":""},
+                      'attr_union': {"sep": "|",
+                                    "null_handling":"drop_na",
+                                    "fill_null":""},
+                      'attr_intersection': {"sep": "|",
+                                    "null_handling":"drop_na",
+                                    "fill_null":""},
+                      'attr_weighted_average': {},
+                    }
     
+
     # Sum
     if name == 'attr_addition':
         return('sum')
@@ -119,22 +158,46 @@ def get_corresponding_function(function_declaration):
     
     # TODO:
     # Add the rest of the functions
-    # Examples
 
-    # Andre: La idea es llamar las funciones que ya hiciste dentro de
-    # un lambda y devolver eso. Que este metodo no quede con funciones muy complejas adentro
-    if name == 'append': # Mock
-        function_declaration['other_attribute'] # sep = '|'
-        sep = '|'
-        
-        fun = lambda s : sep.join(s)
+    # Returns a list of the values separated by the indicated character.  
+    if name == 'attr_append': 
+        input_params = function_declaration['aggl_parameters'].split(";")
+        params = get_params(default_params[name], input_params)
+        if params["null_handling"] == "drop_na":
+            fun = lambda s : params["sep"].join(s.dropna())
+        else:
+            fun = lambda s : params["sep"].join(s.fillna(params["fill_null"]))
+        return(fun)
+
+    if name == 'attr_union': 
+        input_params = function_declaration['aggl_parameters'].split(";")
+        params = get_params(default_params[name], input_params)
+        if params["null_handling"] == "drop_na":
+            fun = lambda s : attr_union(s.dropna(), params["sep"])
+        else:
+            fun = lambda s : attr_union(s.fillna(params["fill_null"]), params["sep"])
+        return(fun)
+
+    if name == 'attr_intersection': 
+        input_params = function_declaration['aggl_parameters'].split(";")
+        params = get_params(default_params[name], input_params)
+        if params["null_handling"] == "drop_na":
+            fun = lambda s : attr_intersection(s.dropna(), params["sep"])
+        else:
+            fun = lambda s : attr_intersection(s.fillna(params["fill_null"]), params["sep"])
+
         return(fun)
     
-    if name == 'wweighted_average': # Mock
-        col2 = function_declaration['col2']
+    if name == 'attr_weighted_average': 
+        weight = function_declaration['secondary_attr']
+        attr = function_declaration['attr_name']
         
-        fun = lambda df : (df[col2]*df[col]).sum() / df[col2].sum() # <- super mock
+        fun = lambda df : (df[weight]*df[attr]).sum() / df[weight].sum() 
 
+        return(fun)
+
+    if name == 'estimate_gamma_delay':
+        fun = lambda s : estimate_gamma_delay(s)
         return(fun)
     
     
@@ -146,119 +209,42 @@ def get_corresponding_function(function_declaration):
 # Support Functions
 # ------------------
 
-
-def attr_addition(self, attr_name):
-    attrs = list(self.df[attr_name].dropna())
-    return sum(attrs)
-
-def attr_substraction():
-    return NotImplemented
-
-def attr_append(self, attr_name, sep=","):
-    '''
-    Receives list of attributes as string. If sep not specified uses ","
-    e.g:
-        attrs = ["1", "2,1,4,5,7,2", "2,3"]
-        returns ["1", "2", "1", "4", "5", "7", "2", "2", "3"] 
-    '''
-    attrs = list(self.df[attr_name].dropna())
-    attrs = [i.split(sep) for i in attrs]
-    attrs_list = [i for sublist in attrs for i in sublist]
-
-    return attrs_list
-
-def attr_append_int(self, attr_name, sep=","):
-    attrs = list(self.df[attr_name].dropna())
-    attrs_list = self.attr_append(attr_name, sep=sep)
-    attrs_list_int = [int(i) for i in attrs_list]
-
-    return attrs_list
-
-def attr_append_float(self, attr_name, sep=","):
-    attrs = list(self.df[attr_name].dropna())
-    attrs_list = self.attr_append(attr_name, sep=sep)
-    attrs_list_int = [float(i) for i in attrs_list]
-
-    return attrs_list
-
-def attr_union(self, attr_name, sep=","):
+def attr_union(series, sep):
     '''
     Receives list of attributes as string. If sep not specified uses ","
     e.g:
         attrs = ["1", "2,1,4,5,7,2", "2,3"]
         returns ["1", "2", "4", "5", "7", "3"] 
     '''
-    attrs = list(self.df[attr_name].dropna())
-    attrs_list = self.attr_append(attr_name, sep=sep)
-    union = list(set().union(attrs_list))
+    attrs = list(series)
+    final_list = [] 
+    attrs_list = [i.split(sep) for i in attrs]
+    attrs_list = [i for sublist in attrs_list for i in sublist]
+    union = sep.join(list(set().union(attrs_list)))
     
     return union
 
-def attr_union_int(self, attr_name):
-    '''
-    Receives list of attributes as string. If sep not specified uses ","
-    e.g:
-        attrs = ["1", "2,1,4,5,7,2", "2,3"]
-        returns [1, 2, 4, 5, 7, 3] 
-    '''
-    attrs = list(self.df[attr_name].dropna())
-    union = self.attr_union(attrs)
-    union_int = [int(i) for i in union] 
-    return union_int
-
-def attr_intersection(self, attr_name, sep="|"):
+def attr_intersection(series, sep):
     '''
     Receives list of attributes as string. If sep not specified uses ","
     e.g:
         attrs = ["1", "2,1,4,5,7,2", "2,3,1"]
         returns ["1"] 
     '''
-    attrs = list(self.df[attr_name].dropna())
-    attrs = [i.split(sep) for i in attrs]
-    attrs_list = [i for sublist in attrs for i in sublist]
-    intersection = list(set().intersection(attrs_list))
+    attrs = list(series) 
+    attrs_list = [set(i.split(sep)) for i in attrs]
+    intersection = sep.join(list(set().intersection(*attrs_list)))
     return intersection
 
-def attr_intersection_int(self, attr_name):
-    '''
-    Receives list of attributes as string. If sep not specified uses ","
-    e.g:
-        attrs = ["1", "2,1,4,5,7,2", "2,3,1"]
-        returns [1] 
-    '''
-    attrs = list(self.df[attr_name].dropna())
-    intersection = self.attr_intersection(attrs)
-    intersection_int = [int(i) for i in intersection] 
-    return intersection_int
-
-def attr_average(self, attr_name):
-    attrs = list(self.df[attr_name].dropna())
-    avg = sum(attrs) / len(attrs)
-    return avg
-
-def attr_weighted_average(self, attr_name, secondary_attr):
-    attrs = list(self.df[attr_name].fillna(0))
-    weights = list(self.df[secondary_attr].fillna(0))
-    wavg = sum(weights[i] * attrs[i] / sum(attrs) for i in range(len(weights)))
-    return wavg
-
-def is_empty(self, parameters):
-    if isinstance(parameters, str):
-        if parameters == "": return True
-    if isinstance(parameters, float):
-        if np.isnan(parameters): return True
-    
-def estimate_gamma_delay(self, attr_name):
+def estimate_gamma_delay(series):
     try:
-        fit_alpha, fit_loc, fit_beta = stats.gamma.fit(self.df[attr_name], floc = -1)
+        fit_alpha, fit_loc, fit_beta = stats.gamma.fit(series, floc = -1)
     except ValueError:
-        self.log.append(f"WARNING: estimate_gamma_delay failed to agglomerate {self.df.geo_id.unique()[0]}")
         return np.nan
     mean_g =  fit_alpha*fit_beta
     var_g  = fit_alpha*fit_beta**2
 
-    x = np.linspace(-1, self.df[attr_name].max(), 61)
+    x = np.linspace(-1, series.max(), 61)
     pdf_fitted = stats.gamma.pdf(x, *(fit_alpha, fit_loc, fit_beta))
     pdf_list = [str(i) for i in pdf_fitted.tolist()]
     return "|".join(pdf_list)
-    
